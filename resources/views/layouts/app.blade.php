b52e571333ceffddaaf8abc70245bc032a0a37fd
Gerco <gerco@appsharing.nl>
2017-01-12 15:52:00 +0100

    Merge branch 'master' of https://github.com/rubenlievense/Project3XL into gerco
    Conflicts:
    	resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
+
 
 <head>
     <meta charset="utf-8">
@@ -96,75 +96,7 @@
                                     {{ Auth::user()->userable()->first()->firstname }}
                                 @elseif (Auth::user()->userable_type == 'App\Company')
                                     {{ Auth::user()->userable()->first()->name }}
-=======
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', '3XL') }}</title>
-
-        <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}"/>
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css"/>
-        <link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap-datetimepicker.min.css') }}"/>
-        <link rel="stylesheet" type="text/css" href="{{asset('/font-awesome-4.7.0/css/font-awesome.min.css')}}" rel="stylesheet">
-
-        <link rel="stylesheet" type="text/css" href="/css/general.css">
-
-
-        <!-- Scripts -->
-        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-
-        <script>
-            window.Laravel = <?php echo json_encode([
-                    'csrfToken' => csrf_token(),
-            ]); ?>
-        </script>
-    </head>
-    <body>
-        <div id="app">
-            <nav class="navbar navbar-default navbar-static-top">
-                <div class="container">
-                    <div class="navbar-header">
-
-                        <!-- Collapsed Hamburger -->
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                            <span class="sr-only">Toggle Navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-
-                        <!-- Branding Image -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ config('app.name', '3XL') }}
-                        </a>
-                    </div>
-
-                    <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="nav navbar-nav">
-                            @if (!Auth::guest())
-
-                                @if (Auth::user()->userable_type == 'App\Applicant')
-                                    <li>
-                                        <a href="{{ url('/cv') }}">
-                                            Mijn CV's
-                                        </a>
-                                    </li>
-                                @endif
-
-                                @if (Auth::user()->userable_type == 'App\Company')
-                                    <li>
-                                        <a href="{{ url('/vacancy') }}">
-                                            Mijn vacatures
-                                        </a>
-                                    </li>
->>>>>>> 5d8ddab3616f40c79776aaed2be611e6767fc937
                                 @endif
 
                                 <span class="caret"></span>
@@ -198,33 +130,14 @@
         </div>
     </nav>
 
-<<<<<<< HEAD
+
     @yield('content')
 </div>
 
-<!-- Scripts -->
-<script src="/js/general.js"></script>
-<script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
-{{--<script type="text/javascript" src="https://cdn.datatables.net/v/bs/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>--}}
-<script type="text/javascript" src="{{ asset('/js/moment.js') }}"></script>
-<script type="text/javascript" src="{{ asset('/js/bootstrap-datetimepicker.js') }}"></script>
-<script src="{{asset('/Summernote/summernote.js')}}"></script>
-<script src="{{asset('/js/bootstrap-multiselect.js')}}"></script>
 
-
-
-
-<script>
-    $(document).ready(function () {
-        $('input[type="datetime"]').datetimepicker();
-    });
-</script>
-</body>
-
-=======
         <!-- Scripts -->
         <script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/v/bs/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>
+        {{--<script type="text/javascript" src="https://cdn.datatables.net/v/bs/jq-2.2.4/dt-1.10.13/datatables.min.js"></script>--}}
         <script type="text/javascript" src="{{ asset('/js/moment.js') }}"></script>
         <script type="text/javascript" src="{{ asset('/js/bootstrap-datetimepicker.min.js') }}"></script>
 
@@ -239,6 +152,6 @@
             });
         </script>
     </body>
->>>>>>> 5d8ddab3616f40c79776aaed2be611e6767fc937
+
 </html>
 
