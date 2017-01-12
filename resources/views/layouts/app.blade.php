ca7db4b1837af1199e21252b10241dba4dbb7a81
Ruben Lievense <ruben.lievense@hotmail.nl>
2017-01-12 11:45:54 +0100

    Algemene layout update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,6 +91,10 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                    	<a href="{{ url('/home') }}">
+                                            Dashboard
+                                        </a>
+                                    
                                         <a href="{{ url('/account') }}">
                                             Settings
                                         </a>
