6bd6d1c5f3464b1c7e02e2aac620177c55f240a9
redmarbakker <redmarbakker@hotmail.com>
2017-01-12 15:01:50 +0100

    createCV

--- resources/views/cv/create.blade.php
+++ resources/views/cv/create.blade.php
@@ -13,22 +13,21 @@
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/cv/create') }}">
                             {{ csrf_field() }}
 
-                            <div class="form-group{{ $errors->has('date') ? ' has-error' : '' }}">
+                            <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
                                 <label for="email" class="col-md-4 control-label">Date</label>
 
                                 <div class="col-md-6">
                                     <div class="input-group">
-                                        <input id="date" type="datetime" class="form-control" name="date"
-                                               value="" required>
+                                        <input id="title" type="text" class="form-control" name="title" required>
 
                                         <span class="input-group-addon">
                                             <span class="glyphicon glyphicon-calendar"></span>
                                         </span>
                                     </div>
 
-                                    @if ($errors->has('date'))
+                                    @if ($errors->has('title'))
                                         <span class="help-block">
-                                            <strong>{{ $errors->first('date') }}</strong>
+                                            <strong>{{ $errors->first('title') }}</strong>
                                         </span>
                                     @endif
                                 </div>
