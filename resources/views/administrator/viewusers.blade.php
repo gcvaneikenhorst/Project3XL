4a2a0a7a3e593f402d0378db4db2ed0b3922a4bb
Ruben Lievense <ruben.lievense@hotmail.nl>
2017-01-12 13:02:18 +0100

    Frontend

--- resources/views/administrator/viewusers.blade.php
+++ resources/views/administrator/viewusers.blade.php
@@ -6,8 +6,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">&nbsp
-                        Users
+                    <div class="panel-heading">&nbsp;
+                        <div class="pull-left box-header">
+	                        Gebruikers
+                        </div>
                     </div>
                     <div class="panel-body">
                         <table class="table">
