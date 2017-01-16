3f46d2f6fcc1a34f7db0e7b43aff82ff3754f532
NiLSPACE <niels.breuker@hotmail.nl>
2017-01-16 14:57:55 +0100

    Matching: Added num matched components

--- app/Console/Commands/start_match.php
+++ app/Console/Commands/start_match.php
@@ -45,21 +45,26 @@ INNER JOIN users u ON u.userable_id = a.id
 WHERE u.userable_type = "App\\\\Applicant"');
 
 
-        $insert = DB::insert('INSERT INTO vacancy_cvs (cv_id, vacancy_id)
-SELECT cv.id AS "cv_id", v.id AS "vacancy_id" FROM users u 
+        $insert = DB::insert('INSERT INTO vacancy_cvs (cv_id, vacancy_id, num_matches)
+SELECT cv.id AS "cv_id", v.id AS "vacancy_id", COUNT(com.id) AS "num_matches" FROM users u 
 INNER JOIN applicants a ON u.userable_id = a.id
 AND u.userable_type = "App\\\\Applicant"
 INNER JOIN cvs cv ON cv.applicant_id = a.id
 INNER JOIN cv_competence cc ON cc.cv_id = cv.id
 INNER JOIN competences com ON cc.competence_id = com.id
 AND com.id IN (
-	SELECT com.id FROM vacancies v
+    SELECT com.id FROM vacancies v
 	INNER JOIN cv_competence cc2 ON cc2.cv_id = v.id
-	INNER JOIN competences com2 ON cc2.competence_id = com2.id
+    INNER JOIN competences com2 ON cc2.competence_id = com2.id
 )
 INNER JOIN vacancy_competence vc ON vc.competence_id = com.id
 INNER JOIN vacancies v ON v.id = vc.vacancy_id
 WHERE u.enabled = 1
+AND NOT EXISTS (
+	SELECT cv_id, vacancy_id FROM vacancy_cvs_payed
+	WHERE cv_id = cv.id
+	AND vacancy_id = v.id
+)
 GROUP BY cv.id, v.id');
 
 		if($insert == 1) {
