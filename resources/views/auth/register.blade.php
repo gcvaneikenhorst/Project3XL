9ce8073d281a139b690e6977d6601066b94c948e
NiLSPACE <niels.breuker@hotmail.nl>
2017-01-18 13:43:50 +0100

    Added page for superadmin to create new administrators

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -350,32 +350,7 @@
     </div>
 </div>
     <script src="js/auth.js"></script>
-    <script>
-        function validate(control) {
-            if (new RegExp($(control).attr('data-validation')).test($(control).val())) {
-                $(control).parents('.form-group').removeClass('has-error');
-                return true;
-            }
-
-            $(control).parents('.form-group').addClass('has-error');
-            return false;
-        }
-
-        $("form").each(function(ind, form) {
-            $(form).on('submit', function(event) {
-                $(this).find('[data-validation]').each(function () {
-                    if (!validate(this)) {
-                        event.preventDefault()
-                    }
-                })
-            })
-            $(form).find('[data-validation]').each(function () {
-                $(this).on('blur', function() {
-                    validate(this)
-                })
-            })
-        })
-    </script>
+    <script src="/js/form-validation.js"></script>
 @endsection
 
 @section('script')
