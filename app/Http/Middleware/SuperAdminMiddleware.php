a7ab72acae410539317d4be24a62d6594bdbf8de
NiLSPACE <niels.breuker@hotmail.nl>
2017-01-19 09:29:57 +0100

    Uploaded SuperAdminMiddleware

---
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SuperAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        echo $request->user()->userable_id;
+        if ($request->user()->userable_type != 'App\Admin' || $request->user()->userable_id != 1)
+            return redirect('home');
+        return $next($request);
+    }
+}
